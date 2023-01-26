c6386f130bce502037193649ae9ff2231cf7afdd
EPAM\Ajitha_Nalluri <Ajitha_Nalluri@epam.com>
2023-01-26 22:59:18 +0530

    Till Lesson 12 Tasks Partially

---
+++ lesson11/src/main/resources/SOLUTION.sql
@@ -0,0 +1,8 @@
+UPDATE Subject SET grade=5 WHERE name = 'End of Suburbia: Oil Depletion and the Collapse of the American Dream';
+
+UPDATE Student SET groupnumber=8 WHERE name='Tremaine Worvill';
+
+UPDATE Payment SET amount=500 AND student_id=2 WHERE payment_date > CAST('2021-01-01' AS TIMESTAMP) AND type_id=2;
+
+UPDATE Mark SET mark=2 WHERE subject_id=315;
+
---
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -0,0 +1,7 @@
+SELECT
+
+SELECT
+
+SELECT avg(mark) FROM Mark m LEFT JOIN Subject s ON m.subject_id=s.id WHERE s.name='maths';
+
+SELECT min(amount) FROM Payment p LEFT JOIN PaymentType pt ON p.type_id=pt.id WHERE pt.name='WEEKLY';
